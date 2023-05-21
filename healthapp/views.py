51c3e53220ab55c596fa7ad91724865bb578c9a3
prajwal333 <prajwallinkinpark123@gmail.com>
2023-05-22 00:25:44 +0545

    pages added

--- healthapp/views.py
+++ healthapp/views.py
@@ -7,8 +7,13 @@ def index(request):
 def about(request):
     return render(request,'healthapp/about.html')
 def contactus(request):
-    return HttpResponse("This is contact us"),
+    return HttpResponse("This is contact us")
 def consultationform(request):
-    return render(request, 'healthapp/consultationform.html'),
+    return render(request, 'healthapp/consultationform.html')
 def blog(request):
     return render(request, 'healthapp/blog.html')
+def ourservices(request):
+    return render(request, 'healthapp/ourservices.html')
+def ourdoctors(request):
+    return render(request, 'healthapp/ourdoctors.html')
+
